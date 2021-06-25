3eecb553449e32b883e94afbc3f83c090d93bca0
Jose Luis Campaña <macpanda@gmail.com>
2021-06-25 12:28:04 +0200

    mini example

---
+++ cypress/integration/0-test/home.spec.js
@@ -0,0 +1,30 @@
+/// <reference types="cypress" />
+
+describe("home usability", () => {
+  before(() => {
+    cy.visit("/");
+  })
+
+  it("images has src attribute", () => {
+    cy.get('img').each(($image, index, $list) => {
+      cy.wrap($image).should('have.attr', 'src');
+    });
+  });
+
+  it.skip("images has alt attribute", () => {
+    cy.get('img').each(($image, index, $list) => {
+      cy.wrap($image).should('have.attr', 'alt');
+    });
+  });
+
+  it("links has href attribute", () => {
+    cy.get('a').each(($link, index, $list) => {
+      cy.wrap($link).should('have.attr', 'href');
+    });
+  });
+  it("links has title attribute", () => {
+    cy.get('a').each(($link, index, $list) => {
+      cy.wrap($link).should('have.attr', 'title');
+    });
+  });
+});
